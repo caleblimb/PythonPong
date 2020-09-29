71ae56ca7d5a4c1411aaaccb0cc43b5353d7f492
Caleb Limb <caleblimb@gmail.com>
2020-09-28 19:23:20 -0600

    Added Keyboard Support

--- pong.py
+++ pong.py
@@ -1,9 +1,9 @@
 """
 CS 246 Python Repository
-
 This is a basic game of Pong that plays in the console.
 """
 
+import keyboard
 import time
 
 running = False
@@ -12,18 +12,18 @@ def start():
     global running
     running = True
     game_loop()
-    
 
 def game_loop():
     global running
     while running == True:
         tick()
         time.sleep(.03)
-        if False:
-            running = False
 
 def tick():
+    global running
     print("tick")
+    if keyboard.is_pressed(chr(27)):
+        running = False
 
 def main ():
     start()
