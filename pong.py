6500ce37c63b6b2d835e53ce4ecb7b92a3475b1b
Caleb Limb <caleblimb@gmail.com>
2020-09-28 21:22:35 -0600

    Added collision detection to ball

--- pong.py
+++ pong.py
@@ -11,12 +11,13 @@ game_height = 30
 game_width = 100
 
 paddle_height = 3
+paddle_gap = 2
 player_pos = game_height / 2
 computer_pos = game_height / 2
 
 ball_x = game_width / 2.0
 ball_y = game_height / 2.0
-ball_vx = 1.0
+ball_vx = -1.0
 ball_vy = 1.0
 
 #Initializes game variables and conditions
@@ -31,7 +32,7 @@ def game_loop():
     global running
     while running == True:
         tick()
-        time.sleep(.05)
+        time.sleep(.2)
 
 #Updates game
 def tick():
@@ -43,11 +44,11 @@ def tick():
 
     #Check for W key
     if keyboard.is_pressed(chr(119)):
-        player_pos -= 0.5
+        player_pos -= 1.0
 
     #Check for S key
     if keyboard.is_pressed(chr(115)):
-        player_pos += 0.5
+        player_pos += 1.0
 
     update_ball()
     draw()
@@ -69,7 +70,7 @@ def draw():
             if y == 0 or y == game_height - 1 or x == 0 or x == game_width - 1:
                 screen_line += '*'
             #Add paddles
-            elif (x == 2 and abs(y - int(player_pos)) < paddle_height) or (x == game_width - 3 and abs(y - int(computer_pos)) < paddle_height):
+            elif (x == paddle_gap and abs(y - int(player_pos)) < paddle_height) or (x == game_width - paddle_gap - 1 and abs(y - int(computer_pos)) < paddle_height):
                 screen_line += '#'
             #Add ball
             elif (x == int(ball_x) and y == int(ball_y)):
@@ -88,15 +89,21 @@ def update_ball():
     global ball_y
     global ball_vx
     global ball_vy
+    global paddle_height
+    global game_width
+    global game_height
 
     ball_x += ball_vx
     ball_y += ball_vy
 
-    if ball_y < 1:
-        ball_vy *= -1
-    if ball_y > game_height - 2:
+    if ball_y < 2 or ball_y > game_height - 3:
         ball_vy *= -1
 
+    if ball_x < paddle_gap + 2 and abs(ball_y - player_pos) < paddle_height:
+        ball_vx *= -1
+
+    if ball_x > game_width - paddle_gap - 2 and abs(ball_y - computer_pos) < paddle_height:
+        ball_vx *= -1
 
 #Main method
 def main ():
