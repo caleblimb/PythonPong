3943d4cb80ba630ba721ad863a6504b0f7978e92
Caleb Limb <caleblimb@gmail.com>
2020-09-28 19:08:38 -0600

    Added game loop

--- pong.py
+++ pong.py
@@ -4,10 +4,29 @@ CS 246 Python Repository
 This is a basic game of Pong that plays in the console.
 """
 
+import time
+
+running = False
+
+def start():
+    global running
+    running = True
+    game_loop()
+    
+
+def game_loop():
+    global running
+    while running == True:
+        tick()
+        time.sleep(.03)
+        if False:
+            running = False
+
+def tick():
+    print("tick")
+
 def main ():
-    print("Hello!")
-
-
+    start()
 
 if __name__ == "__main__":
     main()
